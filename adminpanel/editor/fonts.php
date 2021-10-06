6e5a96ce84be6b594385e6374bc4924d9b67e250
dimaarion <dimaarion@gmail.com>
2021-10-06 21:15:26 +0300

    dimarion/adminpanel.git

--- adminpanel/editor/fonts.php
+++ adminpanel/editor/fonts.php
@@ -3,4 +3,4 @@ header("Access-Control-Allow-Origin:*");
 header("Content-type: application/json");
 require("../admin/classes/Controller.php");
 $controller = new Controller();
-echo($controller->get_json("fonts.json"));
+echo json_encode($controller->get_json("fonts.json")) ;
